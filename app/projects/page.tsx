8ef77722c3462d3402968c3fe0c6f6e059fd6690
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-05-04 13:52:07 +0700

    Test Adding Cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import { title } from "@/components/primitives";
-import { Card, CardFooter, Image, Button } from "@nextui-org/react";
+import React from "react";
+import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 export default function PricingPage() {
   return (
     <div>
@@ -7,26 +8,21 @@ export default function PricingPage() {
         <h1 className={title()}>Projects</h1>
       </div>
       <div>
-        <Card isFooterBlurred radius="lg" className="border-none">
-          <Image
-            alt="Woman listing to music"
-            className="object-cover"
-            height={200}
-            src="https://nextui.org/images/hero-card.jpeg"
-            width={200}
-          />
-          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <p className="text-tiny text-white/80">Available soon.</p>
-            <Button
-              className="text-tiny text-white bg-black/20"
-              variant="flat"
-              color="default"
-              radius="lg"
-              size="sm"
-            >
-              Notify me
-            </Button>
-          </CardFooter>
+        <Card className="py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold">Daily Mix</p>
+            <small className="text-default-500">12 Tracks</small>
+            <h4 className="font-bold text-large">Frontend Radio</h4>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2">
+            <Image
+              alt="Card background"
+              className="object-cover rounded-xl"
+              src="https://nextui.org/images/hero-card-complete.jpeg"
+              width={270}
+			  height={270}
+            />
+          </CardBody>
         </Card>
       </div>
     </div>
